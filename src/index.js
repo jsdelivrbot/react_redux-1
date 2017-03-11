f3ef6bb762997f477fc16fb4726dc40678ee76b8
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-03-11 10:19:47 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+const API_KEY = 'AIzaSyAZ7bCjZHx8YuSLbnEthb2xqIoeR5c9iXo';
+
+
+// Create a new component. This component should produce some HTML.
+const App = () => {
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
+};
+
+// Take this component's generated HTML and put it in the DOM.
+var destination = document.querySelector('.container');
+ReactDOM.render(<App />, destination);
