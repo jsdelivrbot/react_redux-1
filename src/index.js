4ccc83d30b4fe6aaef53f1048e70d7d824efb4ff
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-03-11 14:17:35 -0600

    Add VideoList component

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, data => this.setState({ videos: data }) );
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos });
+    });
   }
 
   render() {
